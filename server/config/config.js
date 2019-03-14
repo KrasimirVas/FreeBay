72a7c8d59d4fb9d414f5a90b51b595716453dc55
KrasimirVas <krasimir_pr@abv.bg>
2019-03-14 15:20:44 +0200

    before changes

--- server/config/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    port: 4200,
-    secret: 'm1 r4nd0m t0k3n',
-    development: {
-        connectionString: 'mongodb://localhost:27017/ThingsForFree'
-    },
-    production: {}
-}
